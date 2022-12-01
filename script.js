97bf63e5c0369540bd9127a87edd5680b01c1e2d
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-01 15:02:49 -0700

    Finished the product without video help, only referenced the remove and querySelectorAll

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 'use strict';
-const showModal = document.querySelector(".show-modal");
-const hidden = document.querySelector(".hidden");
-showModal.addEventListener(`click`, function(){
+// the modal button
+const buttonModal = document.querySelectorAll(".show-modal");
+//hides the modal by using CSS to display none. Also hides the overlay until the modal is clicked.
+const hideModal = document.querySelector(".hidden");
+// creates an X button at the top right of the modal so that you can click out of it
+const closeModal = document.querySelector(".close-modal");
+//this is the overlay that needs to be displayed when show modal is clicked
+const overlay = document.querySelector(".overlay");
+// the message content that is hidden.
+const modal = document.querySelector(".modal");
 
+// add button functionality to open modal
+const openModal = function (){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+
+for (let i = 0; i < buttonModal.length; i++)
+buttonModal[i].addEventListener("click", openModal);
+
+//add button functionality on X of the modal
+const close = function (){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+hideModal.addEventListener("click", close);
+
+// add functionality of escape key being pressed to get out of modal
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape" ){
+       close();
+    };
 });
+
+// add functionality of clicking off modal to close modal
+overlay.addEventListener("click", close);
+
+
+
+
+
+
+
